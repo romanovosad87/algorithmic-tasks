3e103c5d2cfec0149526bd25fade3b59f5519e16
romanovosad87 <roman.novosad@yahoo.com>
2024-08-11 09:40:36 +0200

    41 day commit

---
+++ src/main/java/day_41_1108/Chocolate.java
@@ -0,0 +1,9 @@
+package day_41_1108;
+
+// https://www.codewars.com/kata/534ea96ebb17181947000ada/train/java
+
+public class Chocolate {
+    public static long breakChocolate(long n, long m) {
+        return n * m - 1;
+    }
+}
---
+++ src/main/java/day_41_1108/Solution.java
@@ -0,0 +1,31 @@
+package day_41_1108;
+
+// https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/description/
+
+import java.util.Arrays;
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = {17, 18, 5, 4, 6, 1};
+        System.out.println(Arrays.toString(solution.replaceElements(arr)));
+    }
+
+    public int[] replaceElements(int[] arr) {
+        if (arr.length == 1) {
+            arr[0] = -1;
+            return arr;
+        }
+
+        int max = arr[arr.length - 1];
+        arr[arr.length - 1] = -1;
+        for (int i = arr.length - 2; i >= 0; i--) {
+            var temp = arr[i];
+            arr[i] = max;
+            if (temp > max) {
+                max = temp;
+            }
+        }
+        return arr;
+    }
+}
