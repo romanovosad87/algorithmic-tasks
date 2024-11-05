18b7a3cc303b72ec139c131b60e802f2a5708e0a
roman111 <roman.novosad@tvh.com>
2024-11-05 21:58:25 +0100

    19 day commit

---
+++ src/main/java/secondchallange/day_19_0110/NestedInteger.java
@@ -0,0 +1,12 @@
+package secondchallange.day_19_0110;
+
+import java.util.List;
+
+public interface NestedInteger {
+
+    boolean isInteger();
+
+    Integer getInteger();
+
+    List<NestedInteger> getList();
+}
---
+++ src/main/java/secondchallange/day_19_0110/NestedIterator.java
@@ -0,0 +1,37 @@
+package secondchallange.day_19_0110;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Iterator;
+import java.util.List;
+
+public class NestedIterator implements Iterator<Integer> {
+
+    private Deque<Integer> stack = new ArrayDeque<>();
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        stack = init(nestedList);
+    }
+
+    private Deque<Integer> init(List<NestedInteger> nestedList) {
+        for (var nested : nestedList) {
+            if (nested.isInteger()) {
+                stack.push(nested.getInteger());
+            } else {
+                init(nested.getList());
+            }
+        }
+        return stack;
+    }
+
+    @Override
+    public Integer next() {
+        return stack.pollLast();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
